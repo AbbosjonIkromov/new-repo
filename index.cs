a4fd0ec479b1f32ac7bf8de0e4a21bee9aed3c4e
Abbosjon <abbos6562@gmail.com>
2024-11-21 14:45:36 +0500

    Yangilanish

--- index.cs
+++ index.cs
@@ -7,7 +7,7 @@ namespace Dotnet
         static void Main(string[] args)
         {
             Console.Writeline("Meni familyam Ikromov");
-            Console.Write("Press any to exit...");
+            Console.Write("Press any to exit.");
             Console.ReadKey();
         }
     }
